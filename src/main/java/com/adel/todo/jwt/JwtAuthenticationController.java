f91c6f50dc477ec30d7e126b35f3678f01fb86ae
Adel1250 <adelashrafhamouda@gmail.com>
2024-04-22 20:32:31 +0200

    Version JWT authentication enpoint

--- src/main/java/com/adel/todo/jwt/JwtAuthenticationController.java
+++ src/main/java/com/adel/todo/jwt/JwtAuthenticationController.java
@@ -5,9 +5,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class JwtAuthenticationController {
 
     private final JwtTokenService tokenService;
